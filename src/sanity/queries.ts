3400712984148d3fa5edbb92f7ea85e4cab9b175
Paul Albert <palbert1@gmail.com>
2026-04-04 00:57:41 -0400

    fix: add spread operator to GROQ select() in project detail query

--- src/sanity/queries.ts
+++ src/sanity/queries.ts
@@ -218,7 +218,7 @@ export const PROJECT_DETAIL_QUERY = `
         timestamp
       }
     },
-    select(engagementType == "full-interior-design" => {
+    ...select(engagementType == "full-interior-design" => {
       "procurementItems": procurementItems[] {
         _key,
         name,
